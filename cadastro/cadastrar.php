4f3c28bd1c98019f49c22e39121ad2462b1f8a18
Pam <pamela.saldanha1@aluno.ce.gov.br>
2025-06-12 12:53:53 -0300

    corrigindo problema no action e na senha

--- cadastro/cadastrar.php
+++ cadastro/cadastrar.php
@@ -17,15 +17,13 @@ if (!$nome || !$email || !$senha) {
 if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
     exit("E-mail inválido.");
 }
-
-    $stmtCheck = $conexao->prepare("SELECT id FROM aluno WHERE email = ?");
+    $stmtCheck = $pdo->prepare("SELECT idAluno FROM aluno WHERE email = ?");
     $stmtCheck->execute([$email]);
 
 if ($stmtCheck->rowCount()) {
     exit("Este e-mail já está cadastrado.");
 }
-
-$stmt = $conexao->prepare("INSERT INTO aluno (nome, email, senha) VALUES (?, ?, ?)");
+$stmt = $pdo->prepare("INSERT INTO aluno (nome, email, senha) VALUES (?, ?, ?)");
 if ($stmt->execute([$nome, $email, password_hash($senha, PASSWORD_DEFAULT)])) {
     header('Location: ../login.php');
     exit;
