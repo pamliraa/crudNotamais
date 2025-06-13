7797b13eb8a1cacde92ad70bb18de80c98ee0b17
Pam <pamela.saldanha1@aluno.ce.gov.br>
2025-06-13 10:34:27 -0300

    iniciando o cadastro

---
+++ cadastrarDisciplinas.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once 'conexao.php';
+
+if (!isset($_POST[''], $_POST['email'], $_POST['senha'])) {
+    exit("Acesso inválido.");
+}
+
+    $nome = trim($_POST['nome']);   
+    $email = trim($_POST['email']);
+    $senha = trim($_POST['senha']);
+
+if (!$nome || !$email || !$senha) {
+    exit("Por favor, preencha todos os campos.");
+}
+
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    exit("E-mail inválido.");
+}
+    $stmtCheck = $pdo->prepare("SELECT idAluno FROM aluno WHERE email = ?");
+    $stmtCheck->execute([$email]);
+
+if ($stmtCheck->rowCount()) {
+    exit("Este e-mail já está cadastrado.");
+}
+$stmt = $pdo->prepare("INSERT INTO aluno (nome, email, senha) VALUES (?, ?, ?)");
+if ($stmt->execute([$nome, $email, password_hash($senha, PASSWORD_DEFAULT)])) {
+    header('Location: ../login.php');
+    exit;
+} else {
+    exit("Erro ao cadastrar usuário.");
+}
+?>
