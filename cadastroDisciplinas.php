2557877eae54e7ee026458786bc59342e9eab1e0
Pam <pamela.saldanha1@aluno.ce.gov.br>
2025-06-18 13:04:23 -0300

    estilizando a pagina do cadastro disciplinas

--- cadastroDisciplinas.php
+++ cadastroDisciplinas.php
@@ -1,20 +1,30 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Disciplinas</title>
-    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="styleCadastro.css">
     <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
 </head>
 <body>
-    <h1> Cadastre uma Disciplina:</h1>
-    <form action="cadastrarDisciplinas.php" method="POST">
-        <label for="nome">Nome:</label>
-        <input type="text" id="nome" name="nome">
-        <label for="professor">Professor:</label>
-        <input type="text" id="professor" name="professor">
-        <input type="submit" id="buttom" value="CADASTRAR">
-    </form>
+    <div class="container">
+
+        <h1>Cadastre uma Disciplina:</h1>
+
+        <form action="cadastrarDisciplinas.php" method="POST">
+            <div class="input-group">
+                <label for="nome">Nome:</label>
+                <input type="text" id="nome" name="nome" required>
+            </div>
+
+            <div class="input-group">
+                <label for="professor">Professor:</label>
+                <input type="text" id="professor" name="professor" required>
+            </div>
+
+            <input type="submit" id="buttom" value="CADASTRAR">
+        </form>
+    </div>
 </body>
 </html>
